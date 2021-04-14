42e9b3878087d8d1e6ca74618da88417c9fdd5aa
Gustavo Madureira <gtmadureira@gmail.com>
2021-04-13 22:05:56 -0300

    secp256k1

--- app_python/privkey_to_pubkey/signing_and_verifying.py
+++ app_python/privkey_to_pubkey/signing_and_verifying.py
@@ -10,9 +10,9 @@ import randpass
 
 # Checking the type of Operating System.
 if sys.platform == "linux" or sys.platform == "linux2" or sys.platform == "darwin":
-    def clear(): os.system('clear') # On Linux/OS X System.
+    def clear(): os.system('clear') # On Linux/OS X System
 elif sys.platform == "win32":
-    def clear(): os.system('cls') # On Windows System.
+    def clear(): os.system('cls') # On Windows System
 
 clear()
 
@@ -25,6 +25,13 @@ hashedPassword.update(password.encode())
 
 privKey = int("0x" + hashedPassword.hexdigest(), 16) # Random Private Key at hex_value.
 
+randNum = randpass.numgen(64) # replace with a truly random number.
+
+message = "O Rato roeu a roupa do Rei de Roma" # The message/transaction.
+messageToHash = hashlib.sha3_256()
+messageToHash.update(message.encode())
+hashedMessage = int("0x" + messageToHash.hexdigest(), 16) # The hash of your message/transaction.
+
 # secp256k1 domain parameters.
 Pcurve = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F # The proven prime.
 Acurve = 0x0000000000000000000000000000000000000000000000000000000000000000 # These two defines the elliptic curve.
@@ -34,39 +41,42 @@ Gy = 0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8
 GPoint = (int(Gx), int(Gy)) # This is our generator point.
 N = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141 # Number of points in the field.
 
-def modinv(a, b = int(Pcurve)): # Extended Euclidean Algorithm/'division' in elliptic curves.
+def modinv(a, n = int(Pcurve)): # Extended Euclidean Algorithm/'division' in elliptic curves.
     lm, hm = 1, 0
-    low, high = a % b, b
+    low, high = a % n, n
     while low > 1:
         ratio = high // low
         nm, new = hm - lm * ratio, high - low * ratio
         lm, low, hm, high = nm, new, lm, low
-    return lm % b
+    return lm % n
 
-def ECAdd(a, b): # Point Addition, invented for EC.
-    LambdaAdd = ((b[1] - a[1]) * modinv(b[0] - a[0], int(Pcurve))) % int(Pcurve)
-    x = (LambdaAdd * LambdaAdd - a[0] - b[0]) % int(Pcurve)
-    y = (LambdaAdd * (a[0] - x) - a[1]) % int(Pcurve)
-    return (x, y)
+def ECadd(xp, yp, xq, yq): # Not true addition, invented for EC. It adds Point-P with Point-Q.
+    m = ((yq - yp) * modinv(xq - xp, int(Pcurve)) % int(Pcurve))
+    xr = (m * m - xp - xq) % int(Pcurve)
+    yr = (m * (xp - xr) - yp) % int(Pcurve)
+    return (xr, yr)
 
-def ECDouble(a): # Point Doubling, invented for EC.
-    LamdaDouble = ((3 * a[0] * a[0] + int(Acurve)) * modinv((2 * a[1]), int(Pcurve))) % int(Pcurve)
-    x = (LamdaDouble * LamdaDouble - 2 * a[0]) % int(Pcurve)
-    y = (LamdaDouble * (a[0] - x) - a[1]) % int(Pcurve)
-    return (x, y)
+def ECdouble(xp, yp): # EC point doubling,  invented for EC. It doubles Point-P.
+    LamNumer = 3 * xp * xp + int(Acurve)
+    LamDenom = 2 * yp
+    Lam = (LamNumer * modinv(LamDenom, int(Pcurve))) % int(Pcurve)
+    xr = (Lam * Lam - 2 * xp) % int(Pcurve)
+    yr = (Lam * (xp - xr) - yp) % int(Pcurve)
+    return (xr, yr)
 
-def ECMultiply(GenPoint, privKeyHex): # Doubling & Addition. Not true multiplication.
-    if privKeyHex == 0 or privKeyHex >= N:
-        raise Exception("Invalid Private Key")
-    privKeyBin = str(bin(privKeyHex))[2:]
-    Q = GenPoint
-    for i in range (1, len(privKeyBin)):
-        Q = ECDouble(Q)
-        if privKeyBin[i] == "1":
-            Q = ECAdd(Q, GenPoint)
-    return (Q)
+def EccMultiply(xs, ys, Scalar): # Double & add. EC Multiplication, Not true multiplication.
+    if Scalar == 0 or Scalar >= N:
+        raise Exception("Invalid Scalar/Private Key")
+    ScalarBin = str(bin(Scalar))[2:]
+    Qx, Qy = xs, ys
+    for i in range (1, len(ScalarBin)): # This is invented EC multiplication.
+        Qx, Qy = ECdouble(Qx, Qy) # print "DUB", Qx; print.
+        if ScalarBin[i] == "1":
+            Qx, Qy = ECadd(Qx, Qy, xs, ys) # print "ADD", Qx; print.
+    return (Qx, Qy)
 
-publicKey = ECMultiply(GPoint, privKey)
+xPublicKey, yPublicKey = EccMultiply(Gx, Gy, privKey)
+publicKey = xPublicKey, yPublicKey
 
 print()
 print("--------------------------------------------------------------------------------------------------------------------------------------------")
@@ -108,6 +118,26 @@ if publicKey[1] % 2 == 1: # If the Y coordinate of the Public Key is odd.
     print ("   " + "03" + str(hex(publicKey[0])[2:]).zfill(64).upper())
 else: # If the Y coordinate of the Public Key is even.
     print ("   " + "02" + str(hex(publicKey[0])[2:]).zfill(64).upper())
+print()
+print("--------------------------------------------------------------------------------------------------------------------------------------------")
+print()
+print ("  Signature Generation:")
+xRandSignPoint, yRandSignPoint = EccMultiply(Gx, Gy, randNum)
+r = xRandSignPoint % N
+print()
+print ("   r =", str(hex(r)[2:]).upper())
+s = ((hashedMessage + r * privKey) * (modinv(randNum, N))) % N
+print ("   s =", str(hex(s)[2:]).upper())
+print()
+print("--------------------------------------------------------------------------------------------------------------------------------------------")
+print()
+print ("  Signature Verification:")
+print()
+w = modinv(s, N)
+xu1, yu1 = EccMultiply(Gx, Gy, (hashedMessage * w) % N)
+xu2, yu2 = EccMultiply(xPublicKey, yPublicKey, (r * w) % N)
+x, y = ECadd(xu1, yu1, xu2, yu2)
+print ("  ", r==x)
 print()
 print("--------------------------------------------------------------------------------------------------------------------------------------------")
 print()
