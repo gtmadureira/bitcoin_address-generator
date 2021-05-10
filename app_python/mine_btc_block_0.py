6b7caa2a36970e9c7b5d32cd179ef02ca6001148
Gustavo Madureira <gtmadureira@gmail.com>
2021-05-10 03:11:35 -0300

    updated

--- app_python/mine_btc_block_0.py
+++ app_python/mine_btc_block_0.py
@@ -1,64 +1,79 @@
-import hashlib
-import struct
-import sys
+# Works on Python 3.
 
-VERSION = "01000000" #  set by the network
-PREV_BLOCK = "0000000000000000000000000000000000000000000000000000000000000000" # set by the network
-# Initial target - this is the easiest it will ever be to mine a Bitcoin block
-TARGET = '00000000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF' # set by the network
+import sys
+import struct
+import codecs
+import hashlib
+from dateutil.tz import gettz
+from dateutil.parser import parse
+from datetime import datetime, timedelta, timezone
+
+tzinfos = {"BRST": -7200, "CST": gettz("America/Sao_Paulo")}
+epoch = datetime(1970, 1, 1, tzinfo = timezone.utc)
+
+TARGET = '00000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffff'
 
 number_of_try = 0
+
+def little(string):
+    t = bytearray.fromhex(string)
+    t.reverse()
+    return ''.join(format(x,'02x') for x in t)
 
 def block_hash_less_than_target(block_hash, given_target):
     return int(block_hash, 16) < int(given_target, 16)
 
 def get_little_indian_from_decimal(nonce_decimal):
-    return struct.pack('<I', nonce_decimal).encode('hex')
+    return bytes.decode(codecs.encode(struct.pack('<I', nonce_decimal), 'hex'))
+
+def get_random_nonce_decimal(initial_value):
+    
+    return initial_value + number_of_try
+
+def get_version():
+    version = little("00000001")
+    return version
+
+def get_prev_block():
+    prev_block = little("0000000000000000000000000000000000000000000000000000000000000000")
+    return prev_block    
 
 def get_merkle_root():
-    # this is calculated with the transactions
-    merkle_root = "3ba3edfd7a7b12b27ac72c3e67768f617fc81bc3888a51323a9fb8aa4b1e5e4a"
+    
+    merkle_root = little("4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b")
     return merkle_root
 
 def get_timestamp():
-    # this is your current timestamp
-    timestamp = "29ab5f49" # bigIndian: 495FAB29 -> Epoch: 1231006505 -> GMT : Saturday, January 3, 2009 6:15:05 PM
+    
+    date = "2009-01-03 16:15:05"
+    dt = parse(date + "BRST", tzinfos = tzinfos)
+    ts = (dt - epoch) // timedelta(seconds = 1)
+    timestamp = little(hex(ts)[2:]) 
     return timestamp
 
 def get_size():
-    # this is the size of the block ??
-    size_bits = "ffff001d"
+    
+    size_bits = little("1d00ffff")
     return size_bits
 
-def get_random_nonce_decimal(initial_value):
-    # we Simulate a random number...
-    return initial_value + number_of_try
-
 def get_nonce():
-    # This is a random number
-    nonce_decimal = get_random_nonce_decimal(2083000000) # The corrrect nonce_decimal is 2083236893
+    
+    nonce_decimal = get_random_nonce_decimal(2083230000) 
     nonce_little_indian = get_little_indian_from_decimal(nonce_decimal)
     return nonce_little_indian
 
 def get_header_bin(version, prev_block, merkle_root, timestamp, size_bits, nonce):
-    header_hex = \
-        VERSION + \
-        PREV_BLOCK + \
-        merkle_root + \
-        timestamp + \
-        size_bits + \
-        nonce
+    header_hex = version + prev_block + merkle_root + timestamp + size_bits + nonce
 
-    header_bin = header_hex.decode('hex')
+    header_bin = codecs.decode(header_hex, 'hex')
 
     return header_bin
 
-
 def get_header_second_hash_big_endian_hex(header_bin):
-    first_hash_bin = hashlib.sha256(header_bin).digest()      # af42031e805ff493a07341e2f74ff58149d22ab9ba19f61343e2c86c71c5d66d
-    second_hash_bin = hashlib.sha256(first_hash_bin).digest() # 6fe28c0ab6f1b372c1a6a246ae63f74f931e8365e15a089c68d6190000000000
-    second_hash_big_endian = second_hash_bin[::-1]            # 000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f
-    header_second_hash_big_endian_hex = second_hash_big_endian.encode('hex_codec')
+    first_hash_bin = hashlib.sha256(header_bin).digest()      
+    second_hash_bin = hashlib.sha256(first_hash_bin).digest() 
+    second_hash_big_endian = second_hash_bin[::-1]            
+    header_second_hash_big_endian_hex = bytes.decode(codecs.encode(second_hash_big_endian, 'hex'))
 
     return header_second_hash_big_endian_hex
 
@@ -69,30 +84,30 @@ def print_number_of_try():
 
 def print_solution_found(header_second_hash_big_endian_hex, TARGET):
     print('')
-    print('!!!We found a perfect header combinaison!!!')
+    print('!!! We found a perfect header combination !!!')
     print('because the hash of the header is:  ' + header_second_hash_big_endian_hex)
-    print('and is smaller than the target:     ' + TARGET)
+    print('   and is smaller than the target:  ' + TARGET)
 
-def print_header(version, prev_block, merkle_root, timestamp, size_bits, nonce):
-    print('Base on the header:')
-    print('  version: ' + version)
-    print('  prev_block: ' + prev_block)
-    print('  merkle_root: ' + merkle_root)
-    print('  timestamp: ' + timestamp)
-    print('  size_bits: ' + size_bits)
-    print('  nonce: ' + nonce)
+def print_header(version, prev_block, merkle_root, datetime, timestamp, size_bits, nonce):
+    print('[Base on the header]')
+    print('       VERSION:  ' + version)
+    print('PREVIOUS BLOCK:  ' + prev_block)
+    print('   MERKLE ROOT:  ' + merkle_root)
+    print('     TIMESTAMP:  ' + str(datetime) + " GMT+0000 (" + str(timestamp) + ")")
+    print('          BITS:  ' + size_bits)
+    print('         NONCE:  ' + nonce + " (" + str(int("0x" + nonce, 16)) + ")")
 
 is_solution_found = False
 while not is_solution_found:
-    # We loop until the solution is found...
+    
     print_number_of_try()
 
-    version = VERSION                # "01000000"(set by the network)
-    prev_block = PREV_BLOCK          # "0000000000000000000000000000000000000000000000000000000000000000" (set by the network)
-    merkle_root = get_merkle_root()  # "3ba3edfd7a7b12b27ac72c3e67768f617fc81bc3888a51323a9fb8aa4b1e5e4a"
-    timestamp = get_timestamp()      # "29ab5f49"
-    size_bits = get_size()           # "ffff001d"
-    nonce = get_nonce()              # "1dac2b7c"(integer 2083236893)
+    version = get_version()          
+    prev_block = get_prev_block()    
+    merkle_root = get_merkle_root()  
+    timestamp = get_timestamp()      
+    size_bits = get_size()           
+    nonce = get_nonce()             
 
     header_bin = get_header_bin(version, prev_block, merkle_root, timestamp, size_bits, nonce)
     header_second_hash_big_endian_hex = get_header_second_hash_big_endian_hex(header_bin)
@@ -100,6 +115,7 @@ while not is_solution_found:
 
     if not is_solution_found:
         number_of_try = number_of_try + 1
-    else :
+    else:
         print_solution_found(header_second_hash_big_endian_hex, TARGET)
-        print_header(version, prev_block, merkle_root, timestamp, size_bits, nonce)
+        print_header(little(version), little(prev_block), little(merkle_root),
+        datetime.fromtimestamp(int("0x" + little(timestamp), 16)), int("0x" + little(timestamp), 16), little(size_bits), little(nonce))
